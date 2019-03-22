d2036346df893c66636b6ac7366ae11d0ad7972d
Javier Lete <jglete@gmail.com>
2019-03-22 11:05:09 +0100

    Saludo por consola

--- EjemploGIT/Program.cs
+++ EjemploGIT/Program.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace EjemploGIT
 {
@@ -10,6 +6,7 @@ namespace EjemploGIT
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hola");
         }
     }
 }
